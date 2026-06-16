38cbc40cceef175f47f8526b0058a57a5485c52c
Felipe Luizon <luizonfelipe@gmail.com>
2026-06-16 18:57:08 -0300

    feat: add main application menu

---
+++ main.py
@@ -0,0 +1,38 @@
+contatos = []
+
+while True:
+    print("\n===== Agenda de Contatos =====")
+    print("1 - Adicionar contato")
+    print("2 - Listar contatos")
+    print("3 - Editar contato")
+    print("4 - Marcar/desmarcar contatos favoritos")
+    print("5 - Listar contatos favoritos")
+    print("6 - Remover contato")
+    print("0 - Sair do Programa")
+    
+    opcao = input("\nEscolha uma opção: ")
+    
+    if opcao == "1":
+        print("Função de adicionar contato em breve...")
+        
+    elif opcao == "2":
+        print("Função de listar contatos em breve...")
+        
+    elif opcao == "3":
+        print("Função de editar contato em breve...")
+        
+    elif opcao == "4":
+        print("Função de marcar/desmarcar contatos favoritos em breve...")
+        
+    elif opcao == "5":
+        print("Função de listar contatos favoritos em breve...")
+        
+    elif opcao == "6":
+        print("Função de deletar contato em breve...")
+        
+    elif opcao == "0":
+        print("\nEncerrando programa...")
+        break
+    
+    else:
+        print("\nOpção inválida. Por favor, tente novamente.")
